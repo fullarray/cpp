2bf73388b80f0f0e571a1c09936bc5a65e1d435e
Jonathan <sifreed77@gmail.com>
2018-02-02 21:36:59 -0500

    Added singlyLinked Rep1 implementation

---
+++ handCode/singlyLinkedListR1.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+using namespace std;
+
+struct node{
+  int data;
+  node *next;
+};
+
+class list{
+  private:
+
+  public:
+    list(){
+       head=NULL;
+       tail=NULL;
+    }
+
+    void createNode(int value){
+       node *temp = new node;
+       temp->data = value;
+       temp->next = NULL;
+       if(head == NULL){
+          head = temp;
+          tail = temp;
+          temp = NULL;
+       }else{
+          tail->next = temp;
+          tail = temp;
+       }
+    }
+
+
+    void insertBegin(int value){
+       node *temp = new node;
+       temp->data = value;
+       temp->next = head;
+       head = temp;
+    }
+
+    void insertPosition(int pos, int value){
+       node *pre = new node;
+       node *vur = new node;
+       node *temp = new node;
+       cur = head;
+       for(int i = 1; i < pos; i++){
+          pre = cur;
+          cur = cur->next;  
+       }
+       temp->data = value;
+       pre->next = temp;
+       temp->next = cur;
+    }
+
+    void deleteFirst){
+       node *temp = new node;
+       temp = head;
+       head = head->next;
+       delete temp;
+    }
+
+    void deleteLast(){
+       node *current = new node;
+       node *previous = new node;
+       current = head;
+       while(current->next != null){
+	previous = current;
+        current = current->next;
+      }
+      tail = previous;
+      previous->next = NULL;
+      delete current;
+    }
+
+    void deleteIndex(int pos){
+      node *current = new node;
+      node *previous = new node;
+      current = head;
+      for(int i =1; i<pos; i++){
+	previous = current;
+        current = current->next;
+      }
+      previous->next = current->next;
+    }
+
+    void display(){
+      node *temp = new node;
+      temp = head;
+      while(temp != NULL){
+        cout << temp->data << "\t";
+        temp = temp->next;
+      }
+    }
+    
+    int main(){
+      list o;
+      o.createNode(34);
+      o.createNode(21);
+      o.createNode(98);
+      o.display();
+      return 0;
+    }
+}
